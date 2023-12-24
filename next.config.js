d6fad64fef1679dd3fe2e32fd6294d09bd4d423b
manasroutcse <manasroutcse@gmail.com>
2023-12-24 13:50:58 +0530

    my first next crud app commit
    next crud app

--- next.config.js
+++ next.config.js
@@ -1,4 +1,23 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {}
 
-module.exports = nextConfig
+// // module.exports = nextConfig
+
+// module.exports = {
+//     reactStrictMode: true,
+//   }
+  
+/** @type {import('next').NextConfig} */
+// const nextConfig = {}
+
+// module.exports = nextConfig
+module.exports = {
+    // pageExtensions: ['page.tsx', 'page.ts', 'page.jsx', 'page.js'],
+    reactStrictMode: true
+}
+// {
+//     "compilerOptions": {
+//       "baseUrl": "./pages"
+//     }
+//   }
+  
